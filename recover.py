392deae51b2ea3da201049129de514cdf8769b49
notnew <notnew@no.email>
2014-03-19 13:11:37 -0500

    cleanup
    move comment to docstring
    use with statement
    use newer str.format instead of % string specifier

--- recover.py
+++ recover.py
@@ -1,30 +1,30 @@
 import sys
+import struct
 
 def main():
     if len(sys.argv) < 2:
         usage()
     filename = sys.argv[1]
-    file = open(filename, "rb")
-    print(file.read(4))
-    pos = find_obj(file)
-    print("pos: %s" % pos)
-    file.close()
+    with open(filename, "rb") as file:
+        print(file.read(4))
+        pos = find_obj(file)
+        print("pos: %s" % pos)
 
-# Find a (likely) object in the dbx
-# where the 1st 4 bytes of the object are the offset of the object in the file
 def find_obj(file):
+    """Return the address of the next (likely) object; advance file there.
+
+    The 1st 4 bytes of the object should be the offset of the object in the file
+    """
     pos = file.tell()
-    if pos % 4 != 0:            # ensure we're aligned to a 4 byte word
-        pos = (1 + pos // 4) * 4
-        file.seek(pos)
+    assert(pos % 4 == 0)            # ensure we're aligned to a 4 byte word
+
     while (int.from_bytes(file.read(4), 'little') != pos):
         pos += 4
-        if pos > 100000:
-            break
+    file.seek(pos)
     return pos
 
 def usage():
-    print("Usage: %s <filename>" % sys.argv[0])
+    print("Usage: {} <filename>".format(sys.argv[0]))
     exit()
 
 if __name__ == "__main__":
